7115b8b38635c9778ec3172b47291802596afe6d
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-07-31 19:35:05 +0530

    added frontend directory

--- backend/server.js
+++ backend/server.js
@@ -17,18 +17,18 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
-app.get('/',(req,res)=>{
- res.setHeader("Access-Control-Allow-Credentials","true");
- res.send("API is running")
-})
+// app.get('/',(req,res)=>{
+//  res.setHeader("Access-Control-Allow-Credentials","true");
+//  res.send("API is running")
+// })
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
 // if(process.env.NODE_ENV==='production'){
-// app.use(express.static(path.join(__dirname, './frontend/build')));
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, './frontend/build/index.html'));
-// });
+app.use(express.static(path.join(__dirname, './frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+});
 // }
 // else{
 //  app.get('/',(req,res)=>res.send(' please set to production'))
