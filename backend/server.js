39d476eece39d66652593920f5ec15c5b7408c1d
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-10-22 18:34:45 +0530

    delete the comments

--- backend/server.js
+++ backend/server.js
@@ -17,24 +17,15 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
-// app.get('/',(req,res)=>{
-//  res.setHeader("Access-Control-Allow-Credentials","true");
-//  res.send("API is running")
-// })
+
 app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
-// if(process.env.NODE_ENV==='production'){
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
 });
-// }
-// else{
-//  app.get('/',(req,res)=>res.send(' please set to production'))
-// }
-
-
 
 app.use(errorHandler);
 app.listen(port, () => console.log(`SERVER IS RUNNING ON ${port}`));
