c94c7a5d2a2c747d35bfec7cf96f4c44c410a773
Priya Singh <72970648+singhpriya147@users.noreply.github.com>
2023-11-16 19:51:58 +0530

    moved server.js in root

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const { errorHandler } = require('./middleware/errorMiddleware');
-const postRoutes = require('./routes/postRoutes');
-const userRoutes = require('./routes/userRoutes')
+const { errorHandler } = require('./backend/middleware/errorMiddleware');
+const postRoutes = require('./backend/routes/postRoutes');
+const userRoutes = require('./backend/routes/userRoutes')
 const path =require('path');
 
-const connectDB = require('./config/db');
+const connectDB = require('./backend/config/db');
 const port = process.env.PORT || 5000;
 connectDB();
 
@@ -22,9 +22,9 @@ app.use('/api/posts', postRoutes);
 app.use('/api/users', userRoutes);
 
 
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, './frontend/build')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  res.sendFile(path.join(__dirname, './frontend/build/index.html'));
 });
 
 app.use(errorHandler);
